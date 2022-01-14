c9b00511c005418327ceb8dd17cc44921e5912e9
Soumyadip Payra <payra_941914@student.nitw.ac.in>
2022-01-15 00:21:58 +0530

    msg_q

---
+++ 11_1_22_msgq_poll/Program1/p2.c
@@ -0,0 +1,32 @@
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<sys/types.h>
+
+struct msg_buf {
+
+    long mtype;
+    char mtext[100];
+};
+
+char * message_queue = "./queue";
+
+int main()
+{
+    key_t key = ftok(message_queue,0);
+    int msqid = msgget(key, IPC_CREAT|0666);
+
+    struct msg_buf message;
+
+    message.mtype = 2;
+    sprintf(message.mtext, "%d",getpid());
+    msgsnd(msqid,&message,sizeof(message.mtext),0);
+
+    msgrcv(msqid,&message,sizeof(message.mtext),2,0);
+    printf("%s\n", message.mtext);
+    fflush(stdout);
+
+    // msgctl(msqid,IPC_RMID, NULL);
+}
