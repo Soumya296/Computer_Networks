4960153451164ef9fc47e362a5ccb6e8dec5ec92
Soumyadip Payra <payra_941914@student.nitw.ac.in>
2022-01-27 17:36:51 +0530

    fixed client

--- 27_1_22_socket/program_1/client.c
+++ 27_1_22_socket/program_1/client.c
@@ -7,30 +7,30 @@
 #include <string.h>
 int main()
 {
-    struct sockaddr_in add;
-    socklen_t addr_size;
+    struct sockaddr_in address;
+    socklen_t address_size;
     int sfd=socket(AF_INET,SOCK_STREAM,0);
     if(sfd==0)
     {
         printf("creation of socket failed\n");
     }
-    add.sin_family=AF_INET;
-    add.sin_port=htons(8080);
-    add.sin_addr.s_addr=inet_addr("127.0.0.1");
+    address.sin_family=AF_INET;
+    address.sin_port=htons(8080);
+    address.sin_addr.s_addr=inet_addr("127.0.0.1");
     if(connect(sfd,(struct sockaddr *)&add,sizeof(add))<0)
     {
         printf("connection failed\n");
         return 0;
     }
-    getsockname(sfd,(struct sockaddr *)&add,&addr_size);
+    getsockname(sfd,(struct sockaddr *)&address,&address_size);
     char ip[100];
-    inet_ntop(AF_INET,&add.sin_addr,ip,sizeof(ip));
+    inet_ntop(AF_INET,&address.sin_addr,ip,sizeof(ip));
     printf("local ip : %s\n",ip);
-    printf("local port %d \n",ntohs(add.sin_port));
-    getpeername(sfd,(struct sockaddr*)&add,&addr_size);
-    inet_ntop(AF_INET,&add.sin_addr,ip,sizeof(ip));
+    printf("local port %d \n",ntohs(address.sin_port));
+    getpeername(sfd,(struct sockaddr*)&address,&address_size);
+    inet_ntop(AF_INET,&address.sin_addr,ip,sizeof(ip));
     printf("foreign ip : %s\n",ip);
-    printf("foreign port %d \n",ntohs(add.sin_port));
+    printf("foreign port %d \n",ntohs(address.sin_port));
     char msg[1024];
     char buf[1024];
     printf("Client : ");
